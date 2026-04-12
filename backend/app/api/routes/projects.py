f6c4a70eaac44de62421a67682a316cee9f6de55
Rishav Singh Solanki <82879807+solankirishav21@users.noreply.github.com>
2026-04-12 18:23:11 +0530

    list user projects endpoint

--- backend/app/api/routes/projects.py
+++ backend/app/api/routes/projects.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
+from typing import List
 
+from app.services.project_service import get_user_projects
 from app.api.deps import get_db
 from app.schemas.project import ProjectCreate, ProjectResponse
 from app.services.project_service import create_project
@@ -16,3 +18,10 @@ def create(project: ProjectCreate,
            user: User = Depends(get_current_user)):
     
     return create_project(db, project, user)
+
+@router.get("", response_model=List[ProjectResponse])
+def list_projects(
+    db: Session = Depends(get_db),
+    user: User = Depends(get_current_user)
+):
+    return get_user_projects(db, user)
--- backend/app/services/project_service.py
+++ backend/app/services/project_service.py
@@ -1,7 +1,10 @@
 from sqlalchemy.orm import Session
+from sqlalchemy import or_
+
 from app.db.models.project import Project
 from app.schemas.project import ProjectCreate
 from app.db.models.user import User
+from app.db.models.task import Task
 
 def create_project(db: Session, project_data: ProjectCreate, user: User):
     project = Project(
@@ -15,3 +18,19 @@ def create_project(db: Session, project_data: ProjectCreate, user: User):
     db.refresh(project)
 
     return project
+
+def get_user_projects(db: Session, user: User):
+    projects = (
+        db.query(Project)
+        .outerjoin(Task, Task.project_id == Project.id)
+        .filter(
+            or_(
+                Project.owner_id == user.id,
+                Task.assignee_id == user.id
+            )
+        )
+        .distinct()
+        .all()
+    )
+
+    return projects
